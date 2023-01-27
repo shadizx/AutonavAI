f8112565ac00877e4b538608fcd0389dfc9db792
Shadi Zoldjalali <shadizold@gmail.com>
2023-01-27 12:07:07 -0800

    Replace window reload with internal reload

--- app/components/AIContainer/AICarController.ts
+++ app/components/AIContainer/AICarController.ts
@@ -8,15 +8,16 @@ export default class AICarController {
   bestCar: Car;
   road: Road;
   traffic: Array<Car>;
-  countdown = 1100;
   carsCollided = 0;
+  countdownStart = 1100;
+  countdown = this.countdownStart;
 
   constructor(
-    carControlType: string = "AI",
+    private carControlType: string = "AI",
     private numberOfCars: number = 150,
     canvasWidth: number = 200,
     private readonly mutationPercent: number = 0.1,
-    trafficRows: string[] = ["010", "101", "110", "011"]
+    private trafficRows: string[] = ["010", "101", "110", "011"]
   ) {
     this.road = new Road(canvasWidth / 2, canvasWidth * 0.9);
     this.cars = generateCars(this.road, numberOfCars, carControlType);
@@ -43,7 +44,7 @@ export default class AICarController {
     this.countdown -= 1;
     if (this.countdown < 0) {
       this.toggleMachineLearning();
-      window.location.reload();
+      this.resetCars();
     }
   }
 
@@ -54,12 +55,13 @@ export default class AICarController {
       this.carsCollided += car.collided ? 1 : 0;
       if (this.carsCollided === this.numberOfCars) {
         this.toggleMachineLearning();
-        window.location.reload();
+        this.resetCars();
       }
     });
   }
 
   toggleMachineLearning() {
+    if (this.carControlType != "AI") return;
     const bestBrainSoFar = localStorage.getItem("bestBrain");
     if (!bestBrainSoFar) {
       this.save(this.bestCar);
@@ -85,6 +87,17 @@ export default class AICarController {
       car.y < highest.y ? car : highest
     );
   };
+
+  resetCars() {
+    console.log("resetting");
+    this.traffic = generateTrafficRows(this.trafficRows, this.road);
+    this.cars = generateCars(this.road, this.numberOfCars, this.carControlType);
+    this.bestCar = this.findBestCar();
+
+    this.cars.forEach((car) => car.update(this.road.borders, this.traffic));
+    this.traffic.forEach((vehicle) => vehicle.update(this.road.borders, []));
+    this.countdown = this.countdownStart;
+  }
 
   update() {
     this.updateCountdown();
